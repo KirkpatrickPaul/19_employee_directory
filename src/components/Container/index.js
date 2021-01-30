ca65bcc32e0b4d68f5b5a9daf3589255d17a685a
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2021-01-29 22:32:10 -0600

    styles cards and containers correctly. Text still needs centered and fonted.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,12 +4,16 @@ import './style.css';
 const Container = ({ children }) => {
   return (
     <div className='container outer-container'>
-      <label className='label1'>Name</label>
-      <label className='label2'>Email</label>
-      <label className='label3'>Phone Number</label>
-      <div className='container inner-container'>{children}</div>
+      <div className='container inner-container'>
+        {' '}
+        <label className="label0"> </label>
+        <label className='label1'>Name</label>
+        <label className='label2'>Email</label>
+        <label className='label3'>Phone Number</label>
+        {children}
+      </div>
     </div>
   );
-}
+};
 
 export default Container;
